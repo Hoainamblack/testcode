cd61822a950829db135238b9228b69cc0e4233d4
Hoainamblack <163976364+Hoainamblack@users.noreply.github.com>
2024-03-19 14:56:02 +0700

    Update BTCODE3.py

--- BTCODE3.py
+++ BTCODE3.py
@@ -1,33 +1,42 @@
-def mergeSort(arr):
-    if len(arr) > 1:
-        mid = len(arr) // 2
-        left_half = arr[:mid]
-        right_half = arr[mid:]
+from ._anvil_designer import Form1Template
+from anvil import *
+import anvil.server 
 
-        mergeSort(left_half)
-        mergeSort(right_half)
 
-        i, j, k = 0, 0, 0
-        while i < len(left_half) and j < len(right_half):
-            if left_half[i] < right_half[j]:
-                arr[k] = left_half[i]
-                i += 1
-            else:
-                arr[k] = right_half[j]
-                j += 1
-            k += 1
+class Form1(Form1Template):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
 
-        while i < len(left_half):
-            arr[k] = left_half[i]
-            i += 1
-            k += 1
+    # Any code you write here will run before the form opens.
 
-        while j < len(right_half):
-            arr[k] = right_half[j]
-            j += 1
-            k += 1
+  def image_1_show(self, **event_args):
+    """This method is called when the Image is shown on the screen"""
+    pass
 
-# Example usage
-arr = [64, 34, 25, 12, 22, 11, 90]
-mergeSort(arr)
-print("Sorted array using Merge Sort:", arr)
+  def button_sort_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    pass
+
+
+
+class Form1(Form1Template):
+    def __init__(self, **properties):
+        # Khởi tạo form
+        self.init_components(**properties)
+        
+        # Đăng ký sự kiện nhấn nút để thực hiện sắp xếp dãy số
+        self.button_sort.set_event_handler('click', self.button_1_click)
+    
+    # Hàm xử lý sự kiện khi nhấn nút
+    def button_1_click(self, **event_args):
+        # Lấy dãy số từ Text Box
+        input_text = self.text_box_1.text
+        # Tách các số bằng khoảng trắng và chuyển thành list
+        number_list = list(map(int, input_text.split()))
+        
+        # Sắp xếp dãy số
+        sorted_numbers = sorted(number_list)
+        
+        # Hiển thị dãy số đã sắp xếp trong Label
+        self.text_box_2.text = "Dãy số sau khi sắp xếp: " + " ".join(map(str, sorted_numbers))
